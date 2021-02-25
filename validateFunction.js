f8d94291a23985ce46b31feac678224a67ff2802
zizi <zizi@zizis-MBP.lan>
2021-02-25 21:39:11 +0200

    fix misunderstanding

--- validateFunction.js
+++ validateFunction.js
@@ -1,13 +1,45 @@
-const ValidateFunction = (input) => {
-    const result = input?.length;
-    if (/^.*-s/i.test(input)) {
-        return (result > 10 && result < 30);
+String.prototype.Validate = function (condition) {
+    const string = this.valueOf("String")
+    const isNot = condition.includes('!');
+    condition = isNot ? condition.slice(1, condition.length) : condition;
+    const hasEmpty = condition.includes('empty');
+    const hasLength = condition.includes('length');
+    const comparisonOperators = ["===", "!==", "!=", ">=", "<=", ">", "<", "==",]
+
+    let result = false;
+
+    if (hasEmpty) {
+        string?.trim().length === 0
+    } else if (hasLength) {
+        let operator, index;
+        for (let i = 0; i < comparisonOperators.length; i++) {
+            if (condition.includes(comparisonOperators[i])) {
+                operator = comparisonOperators[i]
+                index = condition.indexOf(comparisonOperators[i])
+                break;
+            }
+        }
+        const num = condition.slice(index + operator.length, condition.length)
+        result = eval(string.length + operator + num)
+    } else {
+        condition = condition.split("/")
+        const matcher = new RegExp(condition[1], condition[2]);
+        result = matcher.test(string);
     }
-    return false;
+    return isNot ? !result : result
 };
-console.log(ValidateFunction()); // false
-console.log(ValidateFunction("d-s")); //false
-console.log(ValidateFunction("al;dkfjaoiwevnwjadadfadsfasdfenv;dskvnaoiwev;lkdsfjaaoiwevewvn-s")); //false
-console.log(ValidateFunction("kkkkkkkddkkkkkkkkkkkk-s")); //true
-console.log(ValidateFunction("jsldi-s")); //false
-console.log(ValidateFunction("aide")); //false
+
+console.log('asdfadsfadsf-s'.Validate('empty'))//false
+console.log('asdfadsfadsf-s'.Validate('!empty'))//true
+console.log('asdfadsfadsf-s'.Validate('length>30'))///false
+console.log('asdfadsfadsf-s'.Validate('length !== 0'))// false
+console.log('asdfadsfadsf-s'.Validate('!length === 5'))//true
+console.log('asdfadsfadsf-s'.Validate('length >= 5'))//true
+console.log('asdfadsfadsf-s'.Validate('!length < 6'))// ture
+console.log('asdfadsfadsf-s'.Validate('length <= 15'))// ture
+console.log('asdfadsfadsf-s'.Validate('!length == 6'))// ture
+console.log('asdfadsfadsf-s'.Validate('/^.*-s/i'))// ture
+console.log('asdfadsfadsf'.Validate('/^.*-s/i'))// false
+
+
+
